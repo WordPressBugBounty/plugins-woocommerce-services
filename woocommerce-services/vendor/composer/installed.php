db0337120dced55cc18131f3e8e01dcbc06280fe
Sean Murphy <sean@iamseanmurphy.com>
2026-05-18 17:12:14 +0000

    3.6.2

--- woocommerce-services/vendor/composer/installed.php
+++ woocommerce-services/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-trunk',
         'version' => 'dev-trunk',
-        'reference' => 'bafc943b88a56153b2a6341b00c481018b7f1f68',
+        'reference' => '3439d7f6f14dc326b2e57c31a665eec5fe1d6929',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-trunk',
             'version' => 'dev-trunk',
-            'reference' => 'bafc943b88a56153b2a6341b00c481018b7f1f68',
+            'reference' => '3439d7f6f14dc326b2e57c31a665eec5fe1d6929',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
